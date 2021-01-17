61fbe7b4b46f473f6c1b59cd482ad773252fab30
Germey <cqc@cuiqingcai.com>
2021-01-17 21:32:34 +0800

    init

---
+++ imagescrawler/spiders/unsplash.py
@@ -0,0 +1,63 @@
+from scrapy import Spider, Request
+import json
+
+
+class UnsplashSpider(Spider):
+    name = 'unsplash'
+    allowed_domains = ['unsplash.com']
+    start_urls = ['http://unsplash.com/']
+    topics = [
+        'wallpapers',
+        'nature',
+        'people',
+        'architecture',
+        'current-events',
+        'experimental',
+        'fashion',
+        'film',
+        'health',
+        'interiors',
+        'street-photography',
+        'technology',
+        'travel',
+        'textures-patterns',
+        'business-work',
+        'animals',
+        'food-drink',
+        'athletics',
+        'spirituality',
+        'arts-culture',
+        'history',
+    ]
+    page_max = 1000
+    index_url_pattern = 'https://unsplash.com/napi/topics/{topic}/photos?page={page}&per_page={per_page}'
+    detail_url_pattern = 'https://unsplash.com/napi/photos/{id}'
+    custom_settings = {
+        'MONGODB_DATABASE_NAME': 'images',
+        'MONGODB_COLLECTION_NAME_DEFAULT': 'unsplash',
+        'IMAGES_STORE': 'images/unsplash'
+    }
+    
+    def start_requests(self):
+        for topic in self.topics:
+            for page in range(1, self.page_max):
+                index_url = self.index_url_pattern.format(
+                    topic=topic,
+                    page=page,
+                    per_page=10
+                )
+                yield Request(index_url, callback=self.parse_index)
+    
+    def parse_index(self, response):
+        items = json.loads(response.text)
+        for item in items:
+            id = item.get('id')
+            detail_url = self.detail_url_pattern.format(id=id)
+            self.logger.debug('detail url %s', detail_url)
+            yield Request(detail_url, callback=self.parse_detail, priority=10)
+    
+    def parse_detail(self, response):
+        data = json.loads(response.text)
+        
+        print(data)
+        yield data
